fd41a96731b2283bf5289d231e8919a52acf77e4
mozaloom <mzlwm320@gmail.com>
2025-05-19 13:21:57 +0300

    update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -1,20 +1,31 @@
 # agent implementation
 import os
-from dotenv import load_dotenv
-from typing import TypedDict, Annotated
-from langgraph.graph.message import add_messages
-from langchain_core.messages import AnyMessage, HumanMessage, AIMessage
-from langgraph.prebuilt import ToolNode
-from langgraph.graph import START, StateGraph
-from langgraph.prebuilt import tools_condition
-from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
-from retriever import *
+import base64
 from tools import *
-from langchain_community.tools import DuckDuckGoSearchRun
-
+from retriever import *
+from dotenv import load_dotenv
+from typing import List, TypedDict, Annotated, Optional
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage
+from langgraph.graph.message import add_messages
+from langgraph.graph import START, StateGraph, MessagesState
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders import UnstructuredExcelLoader
 
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.0)
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+
+
+
+
+
+
+
 
 
 

4708930932a2a300b06a19f384a2ee8e2a05e298
mozaloom <mzlwm320@gmail.com>
2025-05-19 13:03:32 +0300

    creat agent's files

---
+++ agent/agent.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from typing import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langchain_core.messages import AnyMessage, HumanMessage, AIMessage
+from langgraph.prebuilt import ToolNode
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import tools_condition
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from retriever import *
+from tools import *
+from langchain_community.tools import DuckDuckGoSearchRun
+
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+
+
